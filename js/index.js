6bd3733d88ea96db99a98c6ea307e7ed504f85ce
jonas.huberts <jonas.huberts>
2023-06-14 15:58:53 +0200

    update css/index.css and js/index.js

--- js/index.js
+++ js/index.js
@@ -41,12 +41,3 @@ document.addEventListener("DOMContentLoaded", () => {
     applyTheme(this.value);
   });
 });
-
-document.addEventListener("touchmove", function (e) {
-  e.preventDefault();
-}, { passive: false });
-
-window.addEventListener("scroll", (e) => {
-  e.preventDefault();
-  window.scrollTo(0, 0);
-});
