a889b6ed5f6fd44a7e60080d5d3773d6e084d830
jonas.huberts <jonas.huberts>
2023-06-16 12:01:36 +0200

    update index.html and service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,6 @@
-// Based off of https://github.com/pwa-builder/PWABuilder/blob/main/docs/sw.js
-
-/*
-      Welcome to our basic Service Worker! This Service Worker offers a basic offline experience
-      while also being easily customizeable. You can add in your own code to implement the capabilities
-      listed below, or change anything else you would like.
-
-
-      Need an introduction to Service Workers? Check our docs here: https://docs.pwabuilder.com/#/home/sw-intro
-      Want to learn more about how our Service Worker generation works? Check our docs here: https://docs.pwabuilder.com/#/studio/existing-app?id=add-a-service-worker
-
-      Did you know that Service Workers offer many more capabilities than just offline? 
+/* 
         - Background Sync: https://microsoft.github.io/win-student-devs/#/30DaysOfPWA/advanced-capabilities/06
         - Periodic Background Sync: https://web.dev/periodic-background-sync/
-        - Push Notifications: https://microsoft.github.io/win-student-devs/#/30DaysOfPWA/advanced-capabilities/07?id=push-notifications-on-the-web
-        - Badges: https://microsoft.github.io/win-student-devs/#/30DaysOfPWA/advanced-capabilities/07?id=application-badges
     */
 
 const HOSTNAME_WHITELIST = [
@@ -29,16 +16,9 @@ const getFixedUrl = (req) => {
   var url = new URL(req.url);
 
   // 1. fixed http URL
-  // Just keep syncing with location.protocol
-  // fetch(httpURL) belongs to active mixed content.
-  // And fetch(httpRequest) is not supported yet.
   url.protocol = self.location.protocol;
 
   // 2. add query for caching-busting.
-  // Github Pages served with Cache-Control: max-age=600
-  // max-age on mutable content is error-prone, with SW life of bugs can even extend.
-  // Until cache mode of Fetch API landed, we have to workaround cache-busting with query string.
-  // Cache-Control-Bug: https://bugs.chromium.org/p/chromium/issues/detail?id=453190
   if (url.hostname === self.location.hostname) {
     url.search += (url.search ? "&" : "?") + "cache-bust=" + now;
   }
@@ -118,7 +98,10 @@ self.addEventListener("install", function (event) {
         "assets/svg/arrow_back_ios_FILL0_wght400_GRAD0_opsz48.svg",
         "assets/svg/spa_FILL0_wght300_GRAD0_opsz48.svg",
         "assets/svg/tune_FILL0_wght300_GRAD0_opsz48.svg",
-        // Fügen Sie hier alle Ihre anderen Ressourcen hinzu
+        "assets/sound/ende.mp3",
+        "assets/sound/ende1.mp3",
+        "assets/sound/start.mp3",
+        // Alle Ressourcen
       ]);
     })
   );
